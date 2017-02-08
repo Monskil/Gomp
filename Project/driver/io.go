396d03f04940bd4695b300a4e15c98dfc68a0978
vildursn <vildegjaerum@gmail.com>
2017-02-08 13:59:43 +0100

    Update io.go

--- Project/driver/io.go
+++ Project/driver/io.go
@@ -1,10 +1,3 @@
-package driver  // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.h and driver.go
-/*
-#cgo CFLAGS: -std=c11
-#cgo LDFLAGS: -lcomedi -lm
-#include "io.h"
-*/
-import "C"
 
 package driver  // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.h and driver.go
 /*
