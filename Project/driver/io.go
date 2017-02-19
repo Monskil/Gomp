6a6e408d930200fc19b7a2f0676530497b71852a
vildursn <vildegjaerum@gmail.com>
2017-02-19 18:52:32 +0100

    Update io.go
    Fiksa read bit og read analog til go-ints, og ikke c-ints som det var før

--- Project/driver/io.go
+++ Project/driver/io.go
@@ -20,11 +20,11 @@ func io_clear_bit(channel int){
 }
 
 func io_read_bit(channel int) int{
-  return C.io_read_bit(C.int(channel))
+  return int(C.io_read_bit(C.int(channel)))
 }
 
 func io_read_analog(channel int) int{
-  return C.io_read_analog(C.int(channel))
+  return int(C.io_read_analog(C.int(channel)))
 }
 
 func io_write_analog(channel int, value int){
