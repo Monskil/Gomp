fe4f46676764c2e847ece79aaf9ccfd5690cdb92
mariebhe <marieboehenriksen@gmail.com>
2017-02-24 15:55:11 +0100

    Update main.go
    Press the button_up floor 2 thingie

--- Project/src/main.go
+++ Project/src/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"./driver"
-	//"fmt"
+	"driver"
+	"fmt"
+	def "global"
 	//"time"
 )
 
 func main() {
 	driver.Elevator_init()
+
+	value := 0
+	driver.Set_button_lamp(def.BUTTON_UP, def.FLOOR_2, def.ON)
+	for {
+		value = driver.Get_button_signal(def.BUTTON_UP, def.FLOOR_2)
+		fmt.Println(value)
+		if driver.Get_button_signal(def.BUTTON_UP, def.FLOOR_2) != 0 {
+			fmt.Println("Hello, you pressed button up floor 2 hehe.")
+		}
+	}
+
+	// -- koble på nettverk
+
+	// -- start ta imot bestillinger modus
 }
