e981834e5ad60c5b750877ef370880d7d82689b6
vildursn <vildegjaerum@gmail.com>
2017-03-13 21:20:11 +0100

    EVRYTHING IS FIXED
    jk

--- Project/src/queue/compare_cost.go
+++ Project/src/queue/compare_cost.go
@@ -1,52 +1,29 @@
 package queue
 
 
-//compares the cost for the 3 elevators
+//compares the cost for the n elevators
 // should take in num elevators online
-func compare_cost() {
+func compare_cost(elevator[num_elevators_online] queue.Elev_info, num_elevators_online int) queue.Elev_info.Elev_ip {
 
-	/*
-var value[num_elevators] int
-min_val int
-elev_todo = lowest IP
-for i:= 0 ; i<num_elevators ; i++ {
-	value[i] = Calculate_cost(elev[i])
-if value[i] < min_val { elev_todo = elev[i], min_val = value[i]}}
-Return / push channel who is going to do it
-	*/
-	if ((cost_1 < cost_2) && (cost_1 < cost_3)) {
-		elevator 1 gets it
-	} else if ((cost_2 < cost_1) && (cost_2 < cost_3)) {
-		elevator 2 gets it
-	} else if ((cost_3 < cost_1) && (cost_3 < cost_2)) {
-		elevator 3 gets it
-	} else {
-		elevator with lowest IP gets it
+	lowest_cost := 100 // Calculate_cost(elevator[0])
+	var best_suited_elevator queue.Elev_info
+
+
+	for i:= 0 ; i < num_elevators_online ; i++ {
+		cost := Calculate_cost(elevator[i])
+		
+		if cost == -2 {
+			best_suited_elevator = elevator[i]
+			break
+		} else if cost < lowest_cost { 
+			best_suited_elevator = elevator[i]
+			lowest_cost = cost
+		}
 	}
+	return best_suited_elevator.Elev_ip
 
 }
 
 //Takes in the IP for each elevator, so we can get the "order-list" for each elevator
 //Then we can calculates the cost for each elevator and compare them
 //Assigning the order to the elevator with the lowest cost
-
-//calculates the cost for elevator 1
-func cost_elevator_1(order_list [global.NUM_ORDERS].queue.Order, direction global.Motion_direction_t, destination_floor global.Floor_t, current_floor global.Floor_t) int {
-
-	cost_1 := Calculate_cost(order_list, direction, destination_floor, current_floor)
-
-}
-
-//calculates the cost for elevator 2
-func cost_elevator_2(order_list [global.NUM_ORDERS].queue.Order, direction global.Motion_direction_t, destination_floor global.Floor_t, current_floor global.Floor_t) int {
-
-	cost_2 := Calculate_cost(order_list, direction, destination_floor, current_floor)
-
-}
-
-//calculates the cost for elevator 3
-func cost_elevator_3(order_list [global.NUM_ORDERS].queue.Order, direction global.Motion_direction_t, destination_floor global.Floor_t, current_floor global.Floor_t) int {
-
-	cost_3 := Calculate_cost(order_list, direction, destination_floor, current_floor)
-
-}
