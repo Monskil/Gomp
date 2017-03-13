2e402a97b288d20d83bf56df9c5e247546c5450a
mariebhe <marieboehenriksen@gmail.com>
2017-03-13 04:02:05 +0100

    Update main.go
    Mostly the same.

--- Project/src/main.go
+++ Project/src/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"driver"
-	//fmt"
+	//"fmt"
 	"global"
-	"queue"
 	"network"
+	"queue"
 	//"flag"
 	//"network/bcast"
 	//"network/localip"
 	//"network/peers"
 	//"time"
-	"ordermanager"
 	"fsm"
+	"ordermanager"
 )
 
 /*
@@ -34,18 +34,19 @@ func main() {
 	//Global_order_list [global.NUM_GLOBAL_ORDERS]queue.Order
 
 	new_order_chan := make(chan queue.Order, 10) //Er buffra til 10 for da får alle mulig ebestillinger plass
-	updated_order_chan := make(chan queue.Order,10)
+	updated_order_chan := make(chan queue.Order, 10)
 	external_order_list_chan := make(chan [global.NUM_GLOBAL_ORDERS]queue.Order)
 	internal_order_list_chan := make(chan [global.NUM_INTERNAL_ORDERS]queue.Order)
+	new_order_bool_chan := make(chan bool)
 	//isMaster := make(chan bool)
 	//heQueue := make(chan [global.NUM_GLOBAL_ORDERS]queue.Order)
 	//heQueue <- global_order_list
 
-	go ordermanager.Detect_button_pressed(new_order_chan)
-	go queue.Handle_orders(new_order_chan, updated_order_chan, external_order_list_chan, internal_order_list_chan)
 	go network.Network_info()
+	go fsm.State_handler(new_order_bool_chan, updated_order_chan, external_order_list_chan, internal_order_list_chan)
+	go queue.Handle_orders(new_order_bool_chan, new_order_chan, updated_order_chan, external_order_list_chan, internal_order_list_chan)
+	go ordermanager.Detect_button_pressed(new_order_chan)
 	//go ordermanager.HandleNewGlobal(external_order_list_chan, internal_order_list_chan)
-	go fsm.State_handler(updated_order_chan, external_order_list_chan, internal_order_list_chan)
 
 	for { /*
 			select {
