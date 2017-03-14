d46391fe78c3cc037356a83860e28af109c8fc59
vildursn <vildegjaerum@gmail.com>
2017-03-14 05:43:03 +0100

    Update ordermanager.go
    Removed comments and stuff.
    Working.

--- Project/src/ordermanager/ordermanager.go
+++ Project/src/ordermanager/ordermanager.go
@@ -8,20 +8,8 @@ import (
 	"time"
 )
 
-//-- We must make a function for this..
-
-func Make_new_order(button global.Button_t, floor global.Floor_t, state global.Order_state, elev global.Assigned_to)queue.Order{
-	var order queue.Order
-	order.Button = button
-	order.Floor = floor
-	order.Order_state = state
-	order.Assigned_to = elev //Her bør kanskje kost-funksjonen kjøres?? Iallefall hvis heisens state er Inactive
-	return order
-	//PÅ ALLE COMMAND, ASSIGNED TO BØR VÆRE SEG SELV.
-}
-
-func Detect_button_pressed(new_order_chan chan queue.Order) {
-	fmt.Println("Running: Detect button pressed. ")
+func Button_handler(new_order_chan chan queue.Order) {
+	fmt.Println("Running: Button handler. ")
 	var order queue.Order
 
 	for {
@@ -84,61 +72,3 @@ func Detect_button_pressed(new_order_chan chan queue.Order) {
 
 	}
 }
-
-/*func detect_internal_button_pressed() (global.Button_t, bool) {
-	if driver.Get_button_signal(global.BUTTON_COMMAND1) {
-		return global.BUTTON_COMMAND1, true
-	}
-	if driver.Get_button_signal(global.BUTTON_COMMAND2) {
-		return global.BUTTON_COMMAND2, true
-	}
-	if driver.Get_button_signal(global.BUTTON_COMMAND3) {
-		return global.BUTTON_COMMAND3, true
-	}
-	if driver.Get_button_signal(global.BUTTON_COMMAND1) {
-		return global.BUTTON_COMMAND4, true
-	} else {
-		break
-	}
-}*/
-/*
-func Event_management() {
-	/*value := 0
-	driver.Set_button_lamp(global.BUTTON_UP, global.FLOOR_2, global.ON)
-	for {
-		value = driver.Get_button_signal(global.BUTTON_UP, global.FLOOR_2)
-		fmt.Println(value)
-		if driver.Get_button_signal(global.BUTTON_UP, global.FLOOR_2) != 0 {
-			fmt.Println("Hello, you pressed button up floor 2 hehe.")
-		}*/ /*
-	var global_order_list [global.NUM_GLOBAL_ORDERS]queue.Order
-
-	i := 1
-	for i < 3 {
-		b, f, t := Detect_external_button_pressed()
-		//fmt.Println(b, f, t)
-		if t {
-			var order1 = queue.Make_new_order(b, f, queue.Finished, global.ELEV_1)
-			queue.Add_new_global_order(order1, global_order_list)
-			fmt.Println(b, f, t)
-
-		}
-		time.Sleep(1 * time.Second)
-
-	}
-
-	// if button is pressed
-	// -- what should happen?
-}*/
-
-/*
-func test_button(){
-	value := 0
-	driver.Set_button_lamp(global.BUTTON_UP, global.FLOOR_2, global.ON)
-	for {
-		value = driver.Get_button_signal(global.BUTTON_UP, global.FLOOR_2)
-		fmt.Println(value)
-		if driver.Get_button_signal(global.BUTTON_UP, global.FLOOR_2) != 0 {
-			fmt.Println("Hello, you pressed button up floor 2 hehe.")
-		}
-	}*/
